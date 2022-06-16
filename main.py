correct scaling in scenario testing

main.py
@@ -34,48 +34,54 @@ hvac_model = MyGRU(y_parameters=['Energy_consumption'], seq=12, fut=0, x_paramet
 ])
 
 # Scale, split, and sequence the downsampled data frame.
-#x_train, y_train, x_test, y_test = hvac_model.preprocess(raw_data, True)
-
 df_train, x_train, y_train, df_val, x_val, y_val = hvac_model.preprocess(raw_data)
-       
-# Hyperparameter tuning, uncomment these three lines to run it
-#print("Cross-validation hyperparameter tuning")
-#hvac_model.tune_hyperparameters(x=x_train, y=y_train, epochs=1000, max_trials=15)
-#hvac_model.save()
 
-# If everything went OK, find the result folder and append "_hyperparameter_tuning" to the folder name.
+# Enable the following code blocks one at a time and follow the instructions:
+
+# Hyperparameter tuning
+if False:
+    print("Cross-validation hyperparameter tuning")
+    hvac_model.tune_hyperparameters(x=x_train, y=y_train, epochs=1000, max_trials=15)
+    hvac_model.save()
+
+# If everything went OK, find the result folder and append "_hyperparameter_tuning_fut_0" to the folder name.
 
 # Model training using full training data and best hyperparameters found earlier
-hvac_model.load(r'GRU_Energy_consumption_2022-06-14_hyperparameter_tuning')
-print("Train model")
-hvac_model.retrain(x_train, y_train, x_val, y_val)
-hvac_model.save()
+if False:
+    hvac_model.load(r'GRU_Energy_consumption_2022-06-15_hyperparameter_tuning_fut_0')
+    print("Train model")
+    hvac_model.retrain(x_train, y_train, x_val, y_val)
+    hvac_model.save()
 
-# If everything went OK, find the result folder and append "_trained" to the folder name.
+# If everything went OK, find the result folder and append "_trained_fut_0" to the folder name.
 
-################ Iivo's old commented-out code for reference:
+# Calculate statistics on scenario testing results over bootstrapped model training on input sequences resampled with replacement:
 
-# Save the object to folder in the root of the working directory. Uncomment row below, if you have the model trained already.
-#hvac_model.load(r'C:\Users\iivo210\Documents\HAMK_Smart_City\GRU_Inside_temperature_2021-11-05')
+if True:
+    hvac_model.load(r'GRU_Energy_consumption_2022-06-14_trained_fut_0')
 
-# Calculating prediction intervals
-#rounds = 12     # Number of data instances to calculate prediction intervals to.
+    raw_data_offsets_removed = raw_data.copy();
+    raw_data_offsets_removed['Radiator_network_2_temperature'] -= raw_data['Radiator_network_2_offset']
+    raw_data_offsets_removed['Ventilation_network_2_temperature'] -= raw_data['Ventilation_network_2_offset']
+    df_train_offsets_removed, x_train_offsets_removed, y_train_offsets_removed, df_val_offsets_removed, x_val_offsets_removed, y_val_offsets_removed = hvac_model.preprocess(raw_data_offsets_removed, False)
 
-#for i in range(rounds):
-    
-    # Calculating prediction percentiles and saving them to a csv file.
-    #hvac_model.prediction_interval(x_train, y_train, x_test[i])     # NB! The process is computationally intensive.
+    pred_dates_train = hvac_model.get_pred_dates(df_train)
+    pred_dates_val = hvac_model.get_pred_dates(df_val)
+    bootstrap_samples = 20
 
-# Making test predictions with the RNN model.
-#preds = hvac_model.model.predict(x_train)
+    predictedEnergyConsumptionScenarioEnabled = [pred_dates_train.astype('str')]
+    predictedEnergyConsumptionScenarioDisabled = [pred_dates_train.astype('str')]
+    for i in range(bootstrap_samples):
+        train_idx = np.random.choice(range(np.shape(x_train)[0]), size=np.shape(x_train)[0], replace=True) # Draw the training indexes with replacement
+        hvac_model.retrain(x_train[train_idx], y_train[train_idx], x_val, y_val)
+        preds, y_scenario = hvac_model.inv_target(x_train, hvac_model.model.predict(x_train), y_train)
+        preds_offsets_removed, y_offsets_removed = hvac_model.inv_target(x_train_offsets_removed, hvac_model.model.predict(x_train_offsets_removed), y_train_offsets_removed)
+        predictedEnergyConsumptionScenarioEnabled.append(preds.flatten().astype('str'))
+        predictedEnergyConsumptionScenarioDisabled.append(preds_offsets_removed.flatten().astype('str'))
 
-# Loading prediction intervals from disk.
-#low, up = hvac_model.load_intervals(r'C:\Users\iivo210\Documents\HAMK_Smart_City\GRU_Energy_consumption_2021-10-29\pred_ints.csv')
+    predictedEnergyConsumptionScenarioEnabled = np.array(predictedEnergyConsumptionScenarioEnabled).T
+    np.savetxt("predictedEnergyConsumptionScenarioEnabled.csv", predictedEnergyConsumptionScenarioEnabled, fmt='%s', delimiter=',')
 
-# Inverse target variables both for measured values and computed predictions.
-#preds, y_test = hvac_model.inv_target(x_train, preds, y_train)
-#low, up = hvac_model.inv_target(x_test, low, up)
+    predictedEnergyConsumptionScenarioDisabled = np.array(predictedEnergyConsumptionScenarioDisabled).T
+    np.savetxt("predictedEnergyConsumptionScenarioDisabled.csv", predictedEnergyConsumptionScenarioDisabled, fmt='%s', delimiter=',')
 
-# Plot model prediction alongside measured values.
-# Add lower and upper intervals as arguments to plot them.
-#hvac_model.plot_preds(preds, y_test)